db2a415598c80549ce4f24c8765533769dcf2ec8
Hayder Mustafa <hadermusc@gmail.com>
2022-10-12 22:05:21 +0300

    Fix can't compare datetime.datetime to datetime.date error

--- models/approval_request.py
+++ models/approval_request.py
@@ -53,8 +53,8 @@ class ApprovalRequest(models.Model):
 			lambda x:
 				x.request_owner_id == employee_id.user_id and
 				x.category_id.overtime_approval == "by-days" and
-				x.date_end >= start_date and
-				x.date_end <= end_date
+				x.date_end.date() >= start_date and
+				x.date_end.date() <= end_date
 		)
 		total_hours = 0
 		for approval_id in employee_approvals:
